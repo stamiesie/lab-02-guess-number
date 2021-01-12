c070a3d55e99a815f5e5dec253c45ff2f5b6cb17
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-12 13:18:24 -0800

    get DOM elements and add eventListener

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // import functions and grab DOM elements
-
+const guessesLeft = document.getElementById('guesses-left');
+const guessButton = document.getElementById('guess-button');
+const guessResult = document.getElementById('guess-result');
 // initialize state
 
+let numberOfGuesses = 0;
+let correctNumber = Math.ceil(Math.random() * 20);
+
+
 // set event listeners to update state and DOM
+
+guessButton.addEventListener('click', () => {
+    console.log("Hello!");
+})
